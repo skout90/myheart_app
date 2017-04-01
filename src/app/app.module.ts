1feede6f55bd83ea3bf124a799003e106bf30065
Junho Nam <skout90@naver.com>
2017-04-02 04:45:18 +0900

    안드로이드 - 웹에서 보낸 푸시 메세지 수신 기능

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,26 +10,47 @@ import { ScheduleService } from '../providers';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
+const cloudSettings: CloudSettings = {
+    'core': {
+        'app_id': '6788fe53'
+    },
+	'push': {
+	    'sender_id': '421011063140',
+	    'pluginConfig': {
+	        'ios': {
+	            'badge': true,
+	            'sound': true
+	        },
+	        'android': {
+	            'iconColor': '#343434'
+	        }
+	    }
+    }
+};
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    SchedulePage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    SchedulePage
-  ],
-  providers: [
-    ScheduleService,
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        SchedulePage
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp),
+        CloudModule.forRoot(cloudSettings)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        SchedulePage
+    ],
+    providers: [
+        ScheduleService,
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
 export class AppModule {}
--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, Refresher, NavParams } from 'ionic-angular';
+import { NavController, Refresher, NavParams, Platform } from 'ionic-angular';
+import { Push, PushToken } from '@ionic/cloud-angular';
 
 import {ScheduleService} from '../../providers';
 import {Schedule} from '../../models';
 
+
 @Component({
-  selector: 'page-schedule',
-  templateUrl: 'schedule.html'
+    selector: 'page-schedule',
+    templateUrl: 'schedule.html'
 })
 export class SchedulePage implements OnInit {
     schedules: Schedule[] = [];
@@ -14,28 +16,47 @@ export class SchedulePage implements OnInit {
     constructor(
         private scheduleService: ScheduleService,
         public navCtrl: NavController,
-        public navParams: NavParams
+        public navParams: NavParams,
+        public push: Push,
+        public platform: Platform
     ) {}
 
     ionViewDidLoad() {
-        console.log('ionViewDidLoad SchedulePage');
+//        console.log('ionViewDidLoad SchedulePage');
     }
-    
+
     ngOnInit() {
+        console.log(this.platform);
+        if (this.platform.is('android')) {
+            // 푸시 Register
+            this.push.register().then((t: PushToken) => {
+                return this.push.saveToken(t);
+            }).then((t: PushToken) => {
+                console.log('Token saved:', t.token);
+            });
+    
+            this.push.rx.notification()
+                .subscribe((msg) => {
+                    alert(msg.title + ': ' + msg.text);
+            });   
+        } else {
+            console.log('not android');
+        }
+        
         this.loading = true;
         const subscription = this.scheduleService.getSchedules().subscribe(schedules => {
-          console.log(schedules);
-          this.schedules = schedules;
-          this.loading = false;
-          subscription.unsubscribe();
+            console.log(schedules);
+            this.schedules = schedules;
+            this.loading = false;
+            subscription.unsubscribe();
         }, () => this.loading = false);
     }
 
     doRefresh(refresher: Refresher) {
         const subscription = this.scheduleService.getSchedules().subscribe(schedules => {
-          this.schedules = schedules;
-          refresher.complete()
-          subscription.unsubscribe();
+            this.schedules = schedules;
+            refresher.complete()
+            subscription.unsubscribe();
         }, () => refresher.complete());
     }
 
