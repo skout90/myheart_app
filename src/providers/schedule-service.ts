07f949ac21e0ec7eabecf1c700850d761a669ec2
Junho Nam <skout90@naver.com>
2017-05-20 11:49:46 +0900

    API 호출 url, 공통으로 변경/push key 추가

--- src/providers/schedule-service.ts
+++ src/providers/schedule-service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/delay';
 
-import {Schedule} from '../models';
+import { CommonTS } from '../app/commonTS';
+
+import { Schedule } from '../models';
 
 @Injectable()
 export class ScheduleService {
-    constructor(public http: Http) {}
-    
+    constructor(public http: Http) { }
+
+    private baseUrl = CommonTS.BASE_URL;
+
     getSchedules(): Observable<Schedule[]> {
         return this.http
-          .get('http://localhost/schedule/selectScheduleList.do')
-          .delay(2000)
-          .map((res: Response) => res.json());
+            .get(this.baseUrl + '/schedule/list.do')
+            .map((res: Response) => res.json());
     }
 }
